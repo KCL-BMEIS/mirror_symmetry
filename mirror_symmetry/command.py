9bf8973bdfde42621dc2aaf0892cae481b8a0497
David Drobny <d.drobny.17@ucl.ac.uk>
2018-12-14 19:45:40 +0000

    updated help text

--- mirror_symmetry/command.py
+++ mirror_symmetry/command.py
@@ -3,15 +3,25 @@ from .mirror_symmetry_tools import main
 
 
 def process():
-    parser = ArgumentParser(description="Detect mirror symmetry plane")
+    parser = ArgumentParser(description='Detect mirror symmetry plane. The '
+                                        'symmetry plane determined in '
+                                        'point-normal form is printed. '
+                                        'Optional binary mask that splits '
+                                        'the image into the two symmetric '
+                                        'regions can be saved. Optional '
+                                        'two symmetric images can be created '
+                                        'by mirroring the sides across the '
+                                        'symmetry plane.')
 
     parser.add_argument('image',
                         help='Nifti image to be processed')
     parser.add_argument('--save_path', '-p', default=None,
                         help='path to folder in which files are saved.')
     parser.add_argument('--flip_direction', '-d', default=0,
-                        help='Determines the flipping direction initially '
-                             'used, useful to be close to expected symmetry.')
+                        help='The index of the axis used as flipping '
+                             'direction while initialising the registration '
+                             'based symmetry detection. Useful to be close '
+                             'to normal of expected symmetry plane.')
 
     parser.add_argument('--create_mask', '-c', action='store_true',
                         help='Set this flag to save a binary symmetry mask.')
