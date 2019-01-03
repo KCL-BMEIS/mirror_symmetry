92a790265d106d3379778119b54fbab9c2b8de7a
David Drobny <d.drobny.17@ucl.ac.uk>
2019-01-03 15:45:08 +0000

    increasaed version number and development status

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 
 setup(name='mirror_symmetry',
-      version='1.0.0',
+      version='1.1.0',
       description='Tools to detect mirror symmetry in 3D image data.',
       long_description=readme(),
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 4 - Beta',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3',
           'Topic :: 3D image Processing :: Symmetry'],
@@ -21,7 +21,7 @@ setup(name='mirror_symmetry',
       author_email='DaDrobny@gmail.com',
       license='MIT',
       packages=['mirror_symmetry'],
-      install_requires=['numpy', 'nibabel'],
+      install_requires=['numpy>=1.15.0', 'nibabel'],
       entry_points={
         'console_scripts': ['mirror_symmetry_tool = '
                             'mirror_symmetry.command:process']},
